de6054d35faf5a9183bb07aead023166cc793d2f
sfdcselenium01 <sfdcchina01@gmail.com>
2022-03-23 19:25:15 -0500

    batchcommand

---
+++ test.bat
@@ -0,0 +1,12 @@
+@echo off
+echo Hello World from %AGENT_NAME%.
+echo My ID is %AGENT_ID%.
+echo AGENT_WORKFOLDER contents:
+@dir %AGENT_WORKFOLDER%
+echo AGENT_BUILDDIRECTORY contents:
+@dir %AGENT_BUILDDIRECTORY%
+echo BUILD_SOURCESDIRECTORY contents:
+@dir %BUILD_SOURCESDIRECTORY%
+echo SYSTEM_SOURCESDIRECTORY contents:
+@dir %SYSTEM_SOURCESDIRECTORY%
+echo Over and out.
